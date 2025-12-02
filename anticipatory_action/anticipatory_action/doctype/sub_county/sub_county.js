f58f67608db8faf4e0debdd270c157a33c51a6b5
NjengaSheba <njengasheba@gmail.com>
2025-12-02 09:48:47 +0300

    added web form

--- anticipatory_action/anticipatory_action/doctype/sub_county/sub_county.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2025, Kelvin Njenga and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Sub county", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- anticipatory_action/anticipatory_action/doctype/sub_county_aa_details/sub_county_aa_details.py
+++ anticipatory_action/anticipatory_action/doctype/sub_county_aa_details/sub_county_aa_details.py
@@ -5,5 +5,5 @@
 from frappe.model.document import Document
 
 
-class Subcounty(Document):
+class SubCountyAADetails(Document):
 	pass
